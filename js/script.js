d9af9ab46dd3f3a1e645c6b4673750cee7aa0fe3
Ireneusz Miernik <irekmiernik@gmail.com>
2023-03-30 12:02:56 +0200

    Completly new version. Much more usefull

--- js/script.js
+++ js/script.js
@@ -2,14 +2,30 @@ let exchangeRate = document.querySelector(".js-exchangeRate");
 let exchangeValue = document.querySelector(".js-exchangeValue");
 let exchangeValueSelect = document.querySelector(".js-exchangeValueSelect");
 let calculatedValue = document.querySelector(".js-calculatedValue");
-let valueSelect = document.querySelector(".js-valueSelect");
-let formSubmit = document.querySelector(".js-form");
+let calculatedValueSelect = document.querySelector(".js-calculatedValueSelect");
+let formSubmit = document.querySelector(".form");
 
 formSubmit.addEventListener("submit", (event) => {
     event.preventDefault();
-    if (exchangeValueSelect.value === valueSelect.value) { calculatedValue.value = +exchangeValue.value; } else {
-        if (exchangeValueSelect.value === "EURO") { calculatedValue.value = (+exchangeValue.value * exchangeRate.value).toFixed(2); } else {
-            calculatedValue.value = (+exchangeValue.value / +exchangeRate.value).toFixed(2);
-        }
+    if (exchangeValueSelect.value === "EUR") { calculatedValue.value = (+exchangeValue.value * exchangeRate.value).toFixed(2); } else {
+        calculatedValue.value = (+exchangeValue.value / +exchangeRate.value).toFixed(2);
     }
+})
+
+exchangeRate.addEventListener("input", () => {
+    calculatedValue.value = "N/A";
+})
+
+exchangeValue.addEventListener("input", () => {
+    calculatedValue.value = "N/A";
+})
+
+exchangeValueSelect.addEventListener("input", () => {
+    if (exchangeValueSelect.value === "EUR") { calculatedValueSelect.value = "PLN" };
+    if (exchangeValueSelect.value === "PLN") { calculatedValueSelect.value = "EUR" };
+})
+
+calculatedValueSelect.addEventListener("input", () => {
+    if (calculatedValueSelect.value === "EUR") { exchangeValueSelect.value = "PLN" };
+    if (calculatedValueSelect.value === "PLN") { exchangeValueSelect.value = "EUR" };
 })
