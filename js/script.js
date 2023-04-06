b08ae227dadf5d3f64b4aecfcb05aee0eaea5cb3
Ireneusz Miernik <irekmiernik@gmail.com>
2023-04-06 21:04:56 +0200

    New style of button

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 let exchangeRate = document.querySelector(".js-exchangeRate");
 let exchangeValue = document.querySelector(".js-exchangeValue");
-let exchangeValueSelect = document.querySelector(".js-exchangeValueSelect");
+let exchangeValueSelected = document.querySelector(".js-exchangeValueSelected");
 let calculatedValue = document.querySelector(".js-calculatedValue");
-let calculatedValueSelect = document.querySelector(".js-calculatedValueSelect");
+let calculatedValueSelected = document.querySelector(".js-calculatedValueSelected");
 let formSubmit = document.querySelector(".form");
 
 formSubmit.addEventListener("submit", (event) => {
     event.preventDefault();
-    if (exchangeValueSelect.value === "EUR") { calculatedValue.value = (+exchangeValue.value * exchangeRate.value).toFixed(2); } else {
+    if (exchangeValueSelected.value === "EUR") { calculatedValue.value = (+exchangeValue.value * exchangeRate.value).toFixed(2); } else {
         calculatedValue.value = (+exchangeValue.value / +exchangeRate.value).toFixed(2);
     }
 })
@@ -20,12 +20,12 @@ exchangeValue.addEventListener("input", () => {
     calculatedValue.value = "N/A";
 })
 
-exchangeValueSelect.addEventListener("input", () => {
-    if (exchangeValueSelect.value === "EUR") { calculatedValueSelect.value = "PLN" };
-    if (exchangeValueSelect.value === "PLN") { calculatedValueSelect.value = "EUR" };
+exchangeValueSelected.addEventListener("input", () => {
+    if (exchangeValueSelected.value === "EUR") { calculatedValueSelected.value = "PLN" };
+    if (exchangeValueSelected.value === "PLN") { calculatedValueSelected.value = "EUR" };
 })
 
-calculatedValueSelect.addEventListener("input", () => {
-    if (calculatedValueSelect.value === "EUR") { exchangeValueSelect.value = "PLN" };
-    if (calculatedValueSelect.value === "PLN") { exchangeValueSelect.value = "EUR" };
+calculatedValueSelected.addEventListener("input", () => {
+    if (calculatedValueSelected.value === "EUR") { exchangeValueSelected.value = "PLN" };
+    if (calculatedValueSelected.value === "PLN") { exchangeValueSelected.value = "EUR" };
 })
