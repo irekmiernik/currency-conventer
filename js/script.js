50c55415bf24af2027f6695a98208f4d1c6b0f9e
Ireneusz Miernik <irekmiernik@gmail.com>
2023-04-07 10:33:12 +0200

    Script with function settingCurrency(source)

--- js/script.js
+++ js/script.js
@@ -1,31 +1,38 @@
-let exchangeRate = document.querySelector(".js-exchangeRate");
-let exchangeValue = document.querySelector(".js-exchangeValue");
-let exchangeValueSelected = document.querySelector(".js-exchangeValueSelected");
-let calculatedValue = document.querySelector(".js-calculatedValue");
-let calculatedValueSelected = document.querySelector(".js-calculatedValueSelected");
-let formSubmit = document.querySelector(".form");
+{
+    let exchangeCurrency = document.querySelector(".js-exchangeCurrency");
+    let calculatedCurrency = document.querySelector(".js-calculatedCurrency");
 
-formSubmit.addEventListener("submit", (event) => {
-    event.preventDefault();
-    if (exchangeValueSelected.value === "EUR") { calculatedValue.value = (+exchangeValue.value * exchangeRate.value).toFixed(2); } else {
-        calculatedValue.value = (+exchangeValue.value / +exchangeRate.value).toFixed(2);
+    let settingCurrency = (source) => {
+        if (source === "EUR") { return "PLN"; }
+        if (source === "PLN") { return "EUR"; }
     }
-})
 
-exchangeRate.addEventListener("input", () => {
-    calculatedValue.value = "N/A";
-})
+    exchangeCurrency.addEventListener("input", () => {
+        calculatedCurrency.value = settingCurrency(exchangeCurrency.value);
+    })
 
-exchangeValue.addEventListener("input", () => {
-    calculatedValue.value = "N/A";
-})
+    calculatedCurrency.addEventListener("input", () => {
+        exchangeCurrency.value = settingCurrency(calculatedCurrency.value);
+    })
 
-exchangeValueSelected.addEventListener("input", () => {
-    if (exchangeValueSelected.value === "EUR") { calculatedValueSelected.value = "PLN" };
-    if (exchangeValueSelected.value === "PLN") { calculatedValueSelected.value = "EUR" };
-})
+    let exchangeValue = document.querySelector(".js-exchangeValue");
+    let exchangeRate = document.querySelector(".js-exchangeRate");
+    let calculatedValue = document.querySelector(".js-calculatedValue");
+    let formSubmit = document.querySelector(".form");
 
-calculatedValueSelected.addEventListener("input", () => {
-    if (calculatedValueSelected.value === "EUR") { exchangeValueSelected.value = "PLN" };
-    if (calculatedValueSelected.value === "PLN") { exchangeValueSelected.value = "EUR" };
-})
+    formSubmit.addEventListener("submit", (event) => {
+        event.preventDefault();
+        if (exchangeCurrency.value === "EUR") { calculatedValue.value = (+exchangeValue.value * exchangeRate.value).toFixed(2); } else {
+            calculatedValue.value = (+exchangeValue.value / +exchangeRate.value).toFixed(2);
+        }
+    })
+
+    exchangeRate.addEventListener("input", () => {
+        calculatedValue.value = "N/A";
+    })
+
+    exchangeValue.addEventListener("input", () => {
+        calculatedValue.value = "N/A";
+    })
+
+}
