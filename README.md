b975b1acfd75a2fd8dce524e7490a6548a6042a4
Ireneusz Miernik <irekmiernik@gmail.com>
2023-05-15 15:13:02 +0200

    To improve the encapsulation the object 'exchangedRates' is added

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
  ## Demo
  https://irekmiernik.github.io/currency-conventer/
  
- ## Introduction
- There are four currencies (PLN, EUR, USD, GBP) in this conventer.
- Calculations can be made between each pairs of this curriences (for example between EUR and GBP or GBP and EUR) so there can be made twelve differenet calculations.
- The rates of the currencies can be seen and updated by clicking the one of the symbols: EUR, USD, GBP but any of the rates can not be empty.
- To return to the main screen the same symbol has to be click.
+ ## Specification
+ Here is the main screen of this conventer:
+  ![Main screen](images/Main%20sreen.png)
+ There are four currencies (PLN, EUR, USD, GBP) and calculations can be made between each pair of this curriences (for example between EUR and GBP or GBP and EUR) so there can be made twelve differenet calculations. To update the exchange rates of this currencies click the red label: "Tutaj można zmienić kursy walut". Here is the screen to update exchange rates of the currencies: 
+  ![Screen to update exchange rates](images/Scren%20to%20update%20exchange%20rates.png)
+ To return to the main screen the red label: "Kursy walut:", must be clicked but every exchange rates of the curriencies must be greater than zero and can not be empty.
+ At HTML <form> element is used. The Blocks, Elements and Modifiers (BEM) methodology gives CSS code a solid structure that remains simple and easy to understand. At Java Script the encapsulation mechanism: the object "exchangedRates" with the array in it, is defined so the structure and the value of the data is hidden. Beside this it is very easy to add new curriences to this conventer. 
--- js/script.js
+++ js/script.js
@@ -1,142 +1,108 @@
 {
-    const exchangeCurrency = document.querySelector(".js-exchangeCurrency");
-    const exchangeValue = document.querySelector(".js-exchangeValue");
+    const exchangedRates = {
+        ratesTable: [],
+        getIndex(symbol) {
+            for (let i = 0; i < this.ratesTable.length; i++) {
+                if (this.ratesTable[i].currencySymbol === symbol) return i;
+            }
+        },
+        setExchangedRate(symbol, rate) {
+            this.ratesTable[this.getIndex(symbol)].currencyRate = rate;
+        },
+        getExchangedRate(symbol) {
+            return this.ratesTable[this.getIndex(symbol)].currencyRate;
+        }
+    }
 
+    const exchangedCurrency = document.querySelector(".js-exchangedCurrency");
     const calculatedCurrency = document.querySelector(".js-calculatedCurrency");
-    const calculatedValue = document.querySelector(".js-calculatedValue");
+    const exchangedAmount = document.querySelector(".js-exchangedAmount");
+    const calculatedAmount = document.querySelector(".js-calculatedAmount");
 
-    const labelRates = document.querySelector(".js-form__labelRates");
-
-    const labelEUR = document.querySelector(".js-form__labelEUR");
-    const labelRateEUR = document.querySelector(".js-form__labelRateEUR");
-    const rateEUR = document.querySelector(".js-rateEUR");
-
-    const labelUSD = document.querySelector(".js-form__labelUSD");
-    const labelRateUSD = document.querySelector(".js-form__labelRateUSD");
-    const rateUSD = document.querySelector(".js-rateUSD");
-
-    const labelGBP = document.querySelector(".js-form__labelGBP");
-    const labelRateGBP = document.querySelector(".js-form__labelRateGBP");
-    const rateGBP = document.querySelector(".js-rateGBP");
-
-    rateEUR.value = 4.44;
-    rateUSD.value = 3.94;
-    rateGBP.value = 5.40;
-
-
-    const onLabelEURClick = () => {
-        labelRates.hidden = true;
-        labelRateEUR.hidden = false;
-        rateEUR.hidden = false;
+    const calculateRate = (exchangedCurrencySymbol, calculatedCurrencySymbol) => {
+        if (exchangedRates.getExchangedRate(calculatedCurrencySymbol.value) > 0) {
+            return exchangedRates.getExchangedRate(exchangedCurrencySymbol.value) / exchangedRates.getExchangedRate(calculatedCurrencySymbol.value);
+        } else { return 0; }
     }
 
-    const onLabelRateEURClick = () => {
-        if (rateEUR.value > 0) {
-            labelRates.hidden = false;
-            labelRateEUR.hidden = true;
-            rateEUR.hidden = true;
+    const calculate = () => {
+        calculatedAmount.value = "";
+        if ((calculateRate(exchangedCurrency, calculatedCurrency) > 0) & (+exchangedAmount.value > 0)) {
+            calculatedAmount.value = (calculateRate(exchangedCurrency, calculatedCurrency) * +exchangedAmount.value).toFixed(2);
         }
     }
 
-    const onLabelUSDClick = () => {
-        labelRates.hidden = true;
-        labelRateUSD.hidden = false;
-        rateUSD.hidden = false;
-    }
-
-    const onLabelRateUSDClick = () => {
-        if (rateUSD.value > 0) {
-            labelRates.hidden = false;
-            labelRateUSD.hidden = true;
-            rateUSD.hidden = true;
-        }
-    }
-
-    const onLabelGBPClick = () => {
-        labelRates.hidden = true;
-        labelRateGBP.hidden = false;
-        rateGBP.hidden = false;
-    }
-
-    const onlabelRateGBPClick = () => {
-        if (rateGBP.value > 0) {
-            labelRates.hidden = false;
-            labelRateGBP.hidden = true;
-            rateGBP.hidden = true;
-        }
-    }
-
-    const settingCurrency = (source, destination) => {
-        calculatedValue.value = null;
-        if (source.value === destination.value) {
-            if (destination.selectedIndex < 3)
-                destination.selectedIndex += 1;
-            else
-                destination.selectedIndex = 0;
-        }
-    }
-
-    const onExchangeCurrencyInput = () => {
-        settingCurrency(exchangeCurrency, calculatedCurrency);
-        calculationOnAnyInput();
-    }
-
-    const oncCalculatedCurrencyInput = () => {
-        settingCurrency(calculatedCurrency, exchangeCurrency);
-        calculationOnAnyInput();
-    }
-
-    const rateCalculation = () => {
-        var rate = null;
-        calculatedValue.value = null;
-        switch (exchangeCurrency.value) {
-            case "PLN":
-                rate = 1;
-                break;
-            case "EUR":
-                rate = +rateEUR.value;
-                break;
-            case "USD":
-                rate = +rateUSD.value;
-                break;
-            case "GBP":
-                rate = +rateGBP.value;
-                break;
-            default:
-                rate = 0;
+    {
+        const verifyingCurrencies = (source, destination) => {
+            if (source.value === destination.value) {
+                if (destination.selectedIndex < 3) destination.selectedIndex++;
+                else destination.selectedIndex = 0;
+            }
         }
 
-        switch (calculatedCurrency.value) {
-            case "PLN":
-                return rate;
-            case "EUR":
-                return rate /= rateEUR.value;
-            case "USD":
-                return rate /= rateUSD.value;
-            case "GBP":
-                return rate /= rateGBP.value;
-            default:
-                return 0;
+        const onExchangedCurrencyInput = () => {
+            verifyingCurrencies(exchangedCurrency, calculatedCurrency);
+            calculate();
         }
+
+        const onCalculatedCurrencyInput = () => {
+            verifyingCurrencies(calculatedCurrency, exchangedCurrency);
+            calculate();
+        }
+
+        exchangedCurrency.addEventListener("input", onExchangedCurrencyInput);
+        calculatedCurrency.addEventListener("input", onCalculatedCurrencyInput);
+        exchangedAmount.addEventListener("input", calculate);
     }
 
-    const calculationOnAnyInput = () => {
-        if (rateCalculation() > 0 & +exchangeValue.value > 0) {
-            calculatedValue.value = (+exchangeValue.value * rateCalculation()).toFixed(2);
+    {
+        const labelExchangeRates = document.querySelector(".js-labelExchangeRates");
+        const labelInputExchangeRates = document.querySelector(".js-labelInputExchangeRates");
+        const inputExchangeRate = document.querySelector(".js-inputExchangeRate");
+        const inputExchangeCurrency = document.querySelector(".js-inputExchangedCurrency");
+
+        const onLabelExchangeRatesClick = () => {
+            labelExchangeRates.hidden = true;
+            labelInputExchangeRates.hidden = false;
+            inputExchangeRate.hidden = false;
+            inputExchangeCurrency.hidden = false;
+            inputExchangeRate.value = exchangedRates.getExchangedRate(inputExchangeCurrency.value);
         }
+
+        const onLabelInputExchangeRatesClick = () => {
+            if (inputExchangeRate.value > 0) {
+                labelExchangeRates.hidden = false;
+                labelInputExchangeRates.hidden = true;
+                inputExchangeRate.hidden = true;
+                inputExchangeCurrency.hidden = true;
+            } else inputExchangeRate.value = "";
+
+        }
+
+        const onInputExchangedRateInput = () => {
+            if (inputExchangeRate.value > 0) {
+                exchangedRates.setExchangedRate(inputExchangeCurrency.value, inputExchangeRate.value);
+                calculate();
+            }
+        }
+
+        const onInputExchangedCurrencyInput = () => {
+            inputExchangeRate.value = exchangedRates.getExchangedRate(inputExchangeCurrency.value);
+        }
+
+        labelExchangeRates.addEventListener("click", onLabelExchangeRatesClick);
+        labelInputExchangeRates.addEventListener("click", onLabelInputExchangeRatesClick);
+        inputExchangeRate.addEventListener("input", onInputExchangedRateInput);
+        inputExchangeCurrency.addEventListener("input", onInputExchangedCurrencyInput);
     }
 
-    labelEUR.addEventListener("click", onLabelEURClick);
-    labelRateEUR.addEventListener("click", onLabelRateEURClick);
-    labelUSD.addEventListener("click", onLabelUSDClick);
-    labelRateUSD.addEventListener("click", onLabelRateUSDClick);
-    labelGBP.addEventListener("click", onLabelGBPClick);
-    labelRateGBP.addEventListener("click", onlabelRateGBPClick);
+    const inputExchangedRates = (table) => {
+        table.ratesTable.push({ currencySymbol: "PLN", currencyRate: 1.00 });
+        table.ratesTable.push({ currencySymbol: "EUR", currencyRate: 4.56 });
+        table.ratesTable.push({ currencySymbol: "USD", currencyRate: 3.95 });
+        table.ratesTable.push({ currencySymbol: "GBP", currencyRate: 5.30 });
+    }
 
-    exchangeCurrency.addEventListener("input", onExchangeCurrencyInput);
-    calculatedCurrency.addEventListener("input", oncCalculatedCurrencyInput);
-    exchangeValue.addEventListener("input", calculationOnAnyInput);
-    rateEUR.addEventListener("input", calculationOnAnyInput);
-    rateUSD.addEventListener("input", calculationOnAnyInput);
-    rateGBP.addEventListener("input", calculationOnAnyInput);
+    const init = () => { inputExchangedRates(exchangedRates); }
+    init();
 }
